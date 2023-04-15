e2b45d7dd40092f7faf8c6b63c34062de7dedfa0
Alexandr Gerasimov <san4y@yandex.ru>
2023-04-15 13:33:49 +0300

    на сервер

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  app.use(helmet());
+  await app.listen(config().port, () => {
+    console.log(`Server is listening on port: ${config().port}`);
+  });
 }
 bootstrap();
